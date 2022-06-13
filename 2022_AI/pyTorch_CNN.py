d326c0d9620b88229a447c9b9d4da199cd4af650
KAispread <pain0121@naver.com>
2022-06-13 17:35:18 +0900

    pyplot show 'test data'

--- 2022_AI/pyTorch_CNN.py
+++ 2022_AI/pyTorch_CNN.py
@@ -1,62 +1,49 @@
 import torch
 from torch import tensor
-from torchvision import datasets
-from torchvision.transforms import ToTensor
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import Dataset
+
+import torchvision
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+import numpy as np
 import matplotlib.pyplot as plt
-import random
-# MNIST pyplot 으로 그리는 법 (시험 문제)
 
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-# torch.manual_seed(777)
-if device == 'cuda':
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+if device == "cuda":
     torch.cuda.manual_seed_all(777)
-print(device + " is using")
+print("Running on " ,device)
 
-# MNIST 데이터 set 을 가져옴 (training Data) 'Tensor' 라는 자료 구조로
 training_data = datasets.MNIST("data", train=True, download=True, transform=ToTensor())
-
-# MNIST 데이터 set 을 가져옴 (non-traning(test) Data) 'Tensor' 라는 자료 구조로
 test_data = datasets.MNIST("data", train=False, download=True, transform=ToTensor())
 
-# 데이터 set 을 묶어 준다 / 한번에 처리 하는 단위 = batch_size
-# shuffle 로 데이터를 섞어서 학습 시키는 것이 좋음. drop-last 는 100개씩 가져 오다 남은 데이터를 어떻게 할 것인지 지정. false 는 버린다.
 trainLoader = torch.utils.data.DataLoader(training_data, batch_size=100, shuffle=True, drop_last=False)
-testLoader = torch.utils.data.DataLoader(test_data, batch_size=100, shuffle=True, drop_last=False)
+testLoader = torch.utils.data.DataLoader(test_data, batch_size=100, shuffle=False, drop_last=True)
 
-
-class network(nn.Module):  # network model define
+class network(nn.Module):
     def __init__(self):
-        super(network, self).__init__()           # network 구조를 잡아주는 init
-        self.conv = nn.Sequential(                # 소괄호 안의 메서드가 차례대로 실행됨
+        super(network, self).__init__()
+        self.conv = nn.Sequential(
             nn.Conv2d(1, 6, (3, 3), padding=1),
-            # in_channels = 1, out_channels = 6, kernel_size = 3       output = 28 * 28 * 6   6개의 필터 필터당 3*3 배열
             nn.ReLU(),
-            # ReLu 는 음수는 0, 양수는 그대로 표시
-
             nn.MaxPool2d(2, 2),
-            # max 풀링  kernel_size = 2, stride = 2                     output = 14 * 14 * 6 (2*2 라서 기존 28*28이 14*14로 줄어듦)
             nn.Conv2d(6, 16, (3, 3), padding=1),
-            # in_channels = 6, out_channels = 16, kernel_size = 3      output = 14 * 14 * 16
             nn.ReLU(),
             nn.MaxPool2d(2, 2)
-            # max 풀링  kernel_size = 2, stride = 2                     output =  7 *  7 * 16
         )
-        # torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros', device=None, dtype=None)
-        # torch.nn.MaxPool2d( kernel_size , stride = None , padding = 0 , dilation = 1 , return_indices = False , ceil_mode = False )
 
         self.fully_connected = nn.Sequential(
-            nn.Linear(16 * 7 * 7, 120),                 # 1층 레이어  입력층  // 7 * 7 탬플릿 16개를 120개 노드로 만듦
-            nn.Linear(120, 84),                         # 2층 레이어  은닉 층 // 128개의 노드를 84개의 노드와 연결
-            nn.Linear(84, 10)                           # 3층 레이어  결과 층 //  84개의 노드를 10개의 노드와 연결
+            nn.Linear(16 * 7 * 7, 120),
+            nn.Linear(120, 84),
+            nn.Linear(84, 10),
         )
 
-    def forward(self, x):                               # 구조를 잡은 network 를 실행 할 때 수행 하는 동작 forward
+    def forward(self, x):
         x = self.conv(x)
-        x = x.view(-1, 16 * 7 * 7)                      # 1차원 전환  (nn.flatten)
+        x = x.view(-1, 16 * 7 * 7)
         x = self.fully_connected(x)
         return x
 
@@ -65,37 +52,84 @@ def train():
     for epoch in range(Epochs):
         loss_sum = 0
         for data, target in trainLoader:
-            X, y = data.to(device), target.to(device)   # cross
-            optimizer.zero_grad()                       # 기울기를 0으로
-            prediction = model(X)                       # model 에 인스턴스를 삽입하면 결과 출력
-            loss = criterion(prediction, y)             # 그 결과를 타겟과 결과를 비교하여, cross 로스 계산
-            loss.backward()                             # 로스 역전파
-            optimizer.step()                            # 실질적 웨이트 수정
+            X, y = data.to(device), target.to(device)
+            optimizer.zero_grad()
+            prediction = model(X)
+            loss = criterion(prediction, y)
+            loss.backward()
+            optimizer.step()
             loss_sum += loss.item()
-        print("epoch = %d   loss = %f" % (epoch + 1, round(loss_sum / batch_count, 3)))
+        print("Epoch = %d / Loss = %f" % (epoch + 1, round(loss_sum / batch_count, 3)))
         test()
 
 
 def test():
     correct = 0
-    with torch.no_grad():                               # 기울기 구하는 작업 없이 아래 코드를 실행.
+    with torch.no_grad():
         for data, target in testLoader:
             data, target = data.to(device), target.to(device)
-            outputs = model(data)                       # 출력 계산
-            # test는 back propagation 할 필요가 없으므로 코드에서 제외
-            # 추론 계산
-            _, predicted = torch.max(outputs, 1)        # 가장 큰 인덱스 위치를 리턴함  @ return value, index
-            correct += predicted.eq(target).sum()       # 정답과 일치한 경우 정답 카운트를 증가
+            outputs = model(data)
 
-    data_num = len(test_data)  # 데이터 총 건수
-    print("accuracy = {}/10000\n".format(correct))
+            _, predicted = torch.max(outputs, 1)
+            correct += predicted.eq(target).sum()
 
+    percent = round(float(correct/100), 2)
+    print("Accuracy = {}% \n".format(percent))
 
 model = network().to(device)
-optimizer = optim.Adam(model.parameters(), lr=0.001)    # gradiant dist 와 유사한 것
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 criterion = nn.CrossEntropyLoss()
 
 Epochs = 2
 batch_count = len(trainLoader)
 
 train()
+
+
+################################### 트레인 데이터를 활용한 추론
+
+with torch.no_grad():
+  for data, target in trainLoader:
+    X_test, Y_test = data.to(device), target.to(device)  # cross
+    prediction = model(X_test)
+
+  correct_prediction = torch.argmax(prediction, 1) == Y_test
+  accuracy = correct_prediction.float().mean()
+  print('Accuracy:', accuracy.item())
+
+  # r = random.randint(0, len(X_test) - 1)
+  r = 10
+  X_single_data = X_test[r:r + 1].view(-1, 28 * 28).float().to(device)
+  Y_single_data = Y_test[r:r + 1].to(device)
+
+  print('Label: ', Y_single_data.item())
+  X_1 = X_single_data.view(1, 1, 28, 28)
+  single_prediction = model(X_1)
+  print('Prediction: ', torch.argmax(single_prediction, 1).item())
+
+  plt.imshow(torch.Tensor.cpu(X_test[r:r + 1].view(28, 28)), cmap='Greys', interpolation='nearest')
+  plt.show()
+
+
+##################################### 10개의 테스트 데이터 출력
+image, label = next(iter(testLoader))
+
+def imshow(img):
+  img = img / 2 + 0.5
+  npimg = img.numpy()
+  fig = plt.figure(figsize=(10, 5))
+  plt.imshow(np.transpose(npimg, (1, 2, 0)))
+  plt.show()
+
+dataiter = iter(testLoader)
+images, lables = dataiter.next()
+imshow(torchvision.utils.make_grid(images[:10]))
+
+
+##################################### 10번째 테스트 데이터 출력
+### 2D display CPU version
+X_test = test_data.test_data.float()
+r = 9
+
+plt.imshow(X_test[r:r + 1].view(28, 28), cmap='Greys', interpolation='nearest')
+plt.show()
